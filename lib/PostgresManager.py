b41751d6c3ad52d412632fd1cffe2eaec55bc74a
Joseph C. Davis <jdavis@sevone.com>
2020-12-31 14:46:35 -0500

    Added comments.

--- lib/PostgresManager.py
+++ lib/PostgresManager.py
@@ -1,17 +1,23 @@
 import copy
 import psycopg2
 
+# Database connection manager class. 
 class PostgresManager:
 
+    # "static" value represents config section for this class. 
     section = "Postgres"
 
+    # "static" query strings. 
     INSERT_CHAT = '''INSERT INTO chat_log (timestamp, username, action, message) VALUES (%s, %s, %s, %s);'''
 
+    # Takes ConfigurationManager cm as argument. 
+    # Initializes Postgres config section and connects to database. 
     def __init__(self, cm):
         self.cm = copy.deepcopy(cm)
         self.set_config()
         self.connect()
 
+    # Get [Postgres] section from config.ini and set as self.config. 
     def set_config(self):
         if PostgresManager.section in self.cm.parser.sections():
             self.config = self.cm.parser[PostgresManager.section]
@@ -19,6 +25,7 @@ class PostgresManager:
             raise Exception("Invalid configuration file at {config_path}. Missing section '{section}.'"
                 .format(config_path=self.cm.parser.path, section=PostgresManager.section))
 
+    # Connect to database using config values. Set cursor for executing Postgres queries. 
     def connect(self):
         try:
             self.conn = psycopg2.connect(host=self.config['host'], database=self.config['name'], 
@@ -28,6 +35,7 @@ class PostgresManager:
         except Exception:
             raise
 
+    # Execute "INSERT_CHAT" query using args_list
     def insert_chat(self, args_list):
         try:
             self.check_connection() 
@@ -36,21 +44,27 @@ class PostgresManager:
         except:
             raise
 
+    # Check connection and handle retries. 
     def check_connection(self):
         if self.conn is None:
+            print("No database connection... attempting to reconnect.")
             try:
                 retries = self.config['retries']
                 for i in range(1,retries):
+                    print("Retry attempt {retry}...".format(retry=i))
                     self.conn = self.connect()
                     if self.conn is None:
+                        print("Retry attempt {retry} failed...".format(retry=i))
                         continue
                     else:
+                        print("Successfully reconnected!")
                         break;
                 raise Exception("Lost database connection and attempted {n} retries... Failed to reconnect."
                     .format(n=retries))
             except:
                 raise
 
+    # Disconnect from database when class is destroyed. 
     def __del__(self):
         try:
             if self.conn is not None:
