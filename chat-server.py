df6933a2581b41ae2539eff63be75e60b0fb5e07
Joseph C. Davis <jdavis@sevone.com>
2020-12-31 14:01:04 -0500

    Added logging to database of all chat messages - closes #3
    * Added lib/PostgresManager:
        - To load config data from config.ini through lib/ConfigManager
        - Handle db connection and retries
        - Encapsulate INSERT command for chat_log table

--- chat-server.py
+++ chat-server.py
@@ -9,14 +9,23 @@ from enum import Enum
 from lib.PostgresManager import PostgresManager
 from lib.ConfigManager import ConfigManager
 
+import time
+
 ACTION_JOIN = "join"
 ACTION_MESSAGE = "message"
 ACTION_EXIT = "exit"
 
-conf = ConfigManager()
-db = PostgresManager(conf)
+cm = ConfigManager()
+
+#try:
+db = PostgresManager(cm)
+#except Exception as e:
+#    print(e)
 
 USERS = set()
+
+def get_epoch_str():
+    return str(int(time.time()))
 
 def user_join_msg():
     return json.dumps({"action": ACTION_JOIN})
@@ -26,7 +35,13 @@ def user_exit_msg():
 
 async def distribute_message(message):
     if USERS:  
+        username = message.split(':')[0]
         new_message = json.dumps({"action": ACTION_MESSAGE, "message": message})
+        try:
+            db.insert_chat((get_epoch_str(), username, ACTION_MESSAGE, message))
+        except Exception as e:
+            print(e)
+        print(message)
         await asyncio.wait([user.send(new_message) for user in USERS])
 
 async def notify_user_exit():
@@ -40,13 +55,24 @@ async def notify_user_join():
         await asyncio.wait([user.send(message) for user in USERS])
 
 async def register(websocket):
+    username = ACTION_JOIN
+    message = "New user joined chat."
     USERS.add(websocket)
-    print("New user joined chat.")
+    try:
+        db.insert_chat((get_epoch_str(), username, ACTION_JOIN, message))
+    except Exception as e:
+        print(e)
+    print(message)
     await notify_user_join()
-
 async def unregister(websocket):
+    username = ACTION_EXIT
+    message = "User exited..."
     USERS.remove(websocket)
-    print("User exited chat.")
+    try:
+        db.insert_chat((get_epoch_str(), username, ACTION_EXIT, message))
+    except Exception as e:
+        print(e)
+    print(message)
     await notify_user_exit()
 
 async def chat(websocket, path):
@@ -55,15 +81,19 @@ async def chat(websocket, path):
         async for message in websocket:
             data = json.loads(message)
             if data["message"] is not None:
-                print("Sent message: {message}".format(message=data["message"]))
                 await distribute_message(data["message"])
             else:
                 print("Unsupported event: {data}".format(data=data))
     finally:
         await unregister(websocket)
 
-host = "10.0.2.15"
-port = "8888"
+section = "Websocket"
+config = None
+if section in cm.parser.sections():
+    config = cm.parser[section]
+
+host = config['host']
+port = config['port']
 
 start_server = websockets.serve(chat, host, port)
 print("ST-Chat websocket server started at {host}:{port}.".format(host=host, port=port))
