94e68c006c102507a8d16fdf81c6c07deefc81d0
Joseph C. Davis <jdavis@sevone.com>
2020-12-31 10:15:12 -0500

    Removed logging package from st-chat and replaced with print statements - #3
    * Moved logging statements to stdout (print) since the docker will redirect stdout to container's logs.

--- chat-server.py
+++ chat-server.py
@@ -2,10 +2,7 @@
 
 import asyncio
 import json
-import logging
 import websockets
-
-logging.basicConfig()
 
 USERS = set()
 
@@ -32,10 +29,12 @@ async def notify_user_join():
 
 async def register(websocket):
     USERS.add(websocket)
+    print("New user joined chat.")
     await notify_user_join()
 
 async def unregister(websocket):
     USERS.remove(websocket)
+    print("User exited chat.")
     await notify_user_exit()
 
 async def chat(websocket, path):
@@ -44,14 +43,18 @@ async def chat(websocket, path):
         async for message in websocket:
             data = json.loads(message)
             if data["message"] is not None:
-                print(data["message"])
+                print("Sent message: {message}".format(message=data["message"]))
                 await distribute_message(data["message"])
             else:
-                logging.error("unsupported event: {}", data)
+                print("Unsupported event: {data}".format(data=data))
     finally:
         await unregister(websocket)
 
-start_server = websockets.serve(chat, "10.0.2.15", 8888)
+host = "10.0.2.15"
+port = "8888"
+
+start_server = websockets.serve(chat, host, port)
+print("ST-Chat websocket server started at {host}:{port}.".format(host=host, port=port))
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
