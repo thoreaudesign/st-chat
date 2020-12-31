e83a7e3270e87044775b29b8beffbab9c541e676
Joseph C. Davis <jdavis@sevone.com>
2020-12-31 16:25:24 -0500

    Updated comments.

--- nats-sub/__main__.py
+++ nats-sub/__main__.py
@@ -35,29 +35,13 @@ try:
 except Exception as e:
     print(e)
 
-def show_usage():
-    usage = """
-nats-sub SUBJECT [-s SERVER] [-q QUEUE]
-Example:
-nats-sub help -q workers -s nats://127.0.0.1:4222 -s nats://127.0.0.1:4223
-"""
-    print(usage)
-
-def show_usage_and_die():
-    show_usage()
-    sys.exit(1)
-
 async def run(loop):
     parser = argparse.ArgumentParser()
-
-    # e.g. nats-sub hello -s nats://127.0.0.1:4222
     parser.add_argument('subject', default='hello', nargs='?')
     parser.add_argument('-s', '--servers', default=[], action='append')
     parser.add_argument('-q', '--queue', default="")
     parser.add_argument('--creds', default="")
     args = parser.parse_args()
-
-    nc = NATS()
 
     async def error_cb(e):
         print("Error:", e)
@@ -70,30 +54,40 @@ async def run(loop):
     async def reconnected_cb():
         print(f"Connected to NATS at {nc.connected_url.netloc}...")
 
+    # Handle messages received from NATS.
     async def subscribe_handler(msg):
+        
+        # Handles messages from "sport_event" subject.
         if args.subject == "sport_event":
+            # Parse protobuf and deserialize the message into an object.
             deser_msg = event_pb2.event()
             deser_msg.ParseFromString(msg.data)
 
             sport = event_pb2.sport.Name(deser_msg.sport)
             match_title = deser_msg.match_title
             data_event = deser_msg.data_event
-
+            
+            # Print string representation of message.
             print("Message: {sport} | {match_title} | {data_event}".format(
                 sport=sport, match_title=match_title, data_event=data_event))
             
+            # Store message in database.
             db.insert(PostgresManager.INSERT_EVENT,
                 (str(int(time.time())), sport, match_title, data_event))
 
+        # Handles messages from "execution" subject.
         if args.subject == "execution":
+            # Parse protobuf and deserialize the message into an object.
             deser_msg = execution_pb2.execution()
             deser_msg.ParseFromString(msg.data)
 
+            # Print string representation of message.
             print("Message: {symbol} | {market} | {price} | {quantity} | {executionEpoch} | {stateSymbol}".format(
                 symbol=deser_msg.symbol, market=deser_msg.market, price=deser_msg.price,
                 quantity=deser_msg.quantity, executionEpoch=deser_msg.executionEpoch, 
                 stateSymbol=deser_msg.stateSymbol))
 
+            # Store message in database.
             db.insert(PostgresManager.INSERT_EXECUTION,
                 (str(int(time.time())), deser_msg.symbol, deser_msg.market, str(deser_msg.price),
                  str(deser_msg.quantity), str(deser_msg.executionEpoch), deser_msg.stateSymbol))
@@ -108,25 +102,32 @@ async def run(loop):
     if len(args.creds) > 0:
         options["user_credentials"] = args.creds
 
+    # Initialize NATS object.
+    nc = NATS()
+
+    # Asynchronously connect to NATS server.
     try:
         if len(args.servers) > 0:
             options['servers'] = args.servers
-
         await nc.connect(**options)
     except Exception as e:
         print(e)
         show_usage_and_die()
-
     print(f"Connected to NATS at {nc.connected_url.netloc}...")
+
+    # Signal handler for keyboard interrupt. 
     def signal_handler():
         if nc.is_closed:
             return
         print("Disconnecting...")
         loop.create_task(nc.close())
-
+    
+    # Handle keyboard interrupt.
     for sig in ('SIGINT', 'SIGTERM'):
         loop.add_signal_handler(getattr(signal, sig), signal_handler)
 
+    # Subscribe to subject and queue using subscribe_handler callback 
+    # to process events. 
     await nc.subscribe(args.subject, args.queue, subscribe_handler)
 
 if __name__ == '__main__':
