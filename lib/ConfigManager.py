2e9d4ee9b02f7a7d2c7783b9f5c8fe39d4a0b13a
Joseph C. Davis <jdavis@sevone.com>
2021-01-01 18:33:56 -0500

    Added get_config function to ConfigManager.

--- lib/ConfigManager.py
+++ lib/ConfigManager.py
@@ -1,7 +1,7 @@
 import os
 import configparser
 
-class ConfigManager:
+class ConfigManager(object):
 
     file_path = "config.ini"
 
@@ -9,3 +9,12 @@ class ConfigManager:
         self.path = os.path.abspath(ConfigManager.file_path)
         self.parser = configparser.ConfigParser()
         self.parser.read(self.path)
+    
+    # Return config by section
+    def get_config(self, section):
+        if section in self.parser.sections():
+            return self.parser[section]
+        else:
+            raise Exception("Invalid configuration file at {config_path}. Missing section '{section}.'"
+                .format(config_path=self.path, section=section))
+
