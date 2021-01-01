bc1b8335120ed3aba872cd5a917f0e44a6c6fd11
Joseph C. Davis <jdavis@sevone.com>
2021-01-01 18:50:44 -0500

    Rearranged functions to improve readability.

--- chat-server.py
+++ chat-server.py
@@ -9,13 +9,15 @@ import pickle
 
 from lib.PostgresManager import PostgresManager
 from lib.ConfigManager import ConfigManager
-from lib.RedisManager import RedisManager
 
 # Enum emulation. 
 # True Enum type doesn't support string values by default.
 ACTION_JOIN = "join"
 ACTION_MESSAGE = "message"
 ACTION_EXIT = "exit"
+
+# Set of open websocket connections
+WEBSOCKETS = set()
 
 # Initialize config and database managers.
 cm = ConfigManager()
@@ -24,7 +26,48 @@ try:
 except Exception as e:
     print(e)
 
-USERS = set()
+# "Main" function to run in event loop. 
+# Handles user management and message distribution. 
+async def chat(websocket, path):
+    # Randomly generated user name. 
+    username = names.get_full_name()
+    await user_change(username, websocket, ACTION_JOIN)
+    try:
+        async for message in websocket:
+            data = json.loads(message)
+            if data["message"] is not None:
+                await notify_users(username, ACTION_MESSAGE, data["message"])
+            else:
+                print("Unsupported event: {data}".format(data=data))
+    finally:
+        await user_change(username, websocket, ACTION_EXIT)
+
+# Handles connection and disconnection of users. 
+async def user_change(username, websocket, action):
+    message = toggle_connection(username, websocket, action)
+    await notify_users(username, action, message)
+
+# Adds or removes websocket from "users" set. 
+# Returns the appropriate message to inform the users.
+def toggle_connection(username, websocket, action):
+    if action is ACTION_JOIN:
+        WEBSOCKETS.add(websocket)
+        return user_msg_join(username)
+
+    if action is ACTION_EXIT:
+        WEBSOCKETS.remove(websocket)
+        return user_msg_exit(username)
+
+# Distributes messages to all connected users. 
+# No action if there are no connected users. 
+async def notify_users(username, action, message):
+    if WEBSOCKETS:
+        if action is ACTION_MESSAGE:
+            message = "{username}: {message}".format(username=username, message=message)
+        payload = get_payload(ACTION_MESSAGE, message)
+        print(message)
+        log_message((get_epoch_str(), username, ACTION_MESSAGE, message))
+        await asyncio.wait([websocket.send(payload) for websocket in WEBSOCKETS])
 
 # Configuration file management
 def get_config():
@@ -44,52 +87,11 @@ def log_message(attr_list):
 def get_payload(action, message):
     return json.dumps({"action": ACTION_MESSAGE, "message": message})
 
-# Distributes messages to all connected users. 
-# No action if there are no connected users. 
-async def notify_users(username, action, message):
-    if USERS:
-        if action is ACTION_MESSAGE:
-            message = "{username}: {message}".format(username=username, message=message)
-        payload = get_payload(ACTION_MESSAGE, message)
-        print(message)
-        log_message((get_epoch_str(), username, ACTION_MESSAGE, message))
-        await asyncio.wait([user.send(payload) for user in USERS])
-
-# Adds or removes websocket from "users" set
-def toggle_connection(username, websocket, action):
-    if action is ACTION_JOIN:
-        USERS.add(websocket)
-        return user_msg_join(username)
-
-    if action is ACTION_EXIT:
-        USERS.remove(websocket)
-        return user_msg_exit(username)
-
-# Handles connection and disconnection of users. 
-async def user_change(username, websocket, action):
-    message = toggle_connection(username, websocket, action)
-    await notify_users(username, action, message)
-
 def user_msg_join(username):
     return username + " joined chat."
 
 def user_msg_exit(username):
     return username + " exited chat."
-
-# "Main" function to run in event loop. 
-# Handles user management and message distribution. 
-async def chat(websocket, path):
-    username = names.get_full_name()
-    await user_change(username, websocket, ACTION_JOIN)
-    try:
-        async for message in websocket:
-            data = json.loads(message)
-            if data["message"] is not None:
-                await notify_users(username, ACTION_MESSAGE, data["message"])
-            else:
-                print("Unsupported event: {data}".format(data=data))
-    finally:
-        await user_change(username, websocket, ACTION_EXIT)
 
 # Initialize config values from config.ini
 config = get_config()
